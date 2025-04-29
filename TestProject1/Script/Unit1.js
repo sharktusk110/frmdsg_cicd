0895aff3f346f7b51c4083199c49ee6b8e598e2f
sharktusk110 <syedshariq.abbas@aristocrat.com>
2025-04-29 13:35:46 +0530

    pro_del

--- TestProject1/Script/Unit1.js
+++
@@ -1,8 +0,0 @@
-﻿
-function test() {
-  
-  Aliases.browser.WaitPage("*");
-  Aliases.browser.Page("*").WaitElement("//p[contains(text(), 'In order to log in Orders sample use the following information:')]")
-  
-  
-}
